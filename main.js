fa65e611fd6474028749a857e755d3aa3f11686f
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-08 01:52:00 +0900

    :sparkles: ADD: routes `/items/:vegetable`

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const port = 3000,
   express = require("express"),
   app = express();
 
-app
-  .get("/", (req, res) => {
-    res.send("Hello, Universe!");
-  })
-  .listen(port, () => {
-    console.log(
-      `The express.js server has started and is listening on port number: ${port}`
-    );
-  });
+app.get("/items/:vegetable", (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+});
+
+app.listen(port, () => {
+  console.log(
+    `The express.js server has started and is listening on port number: ${port}`
+  );
+});

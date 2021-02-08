e31381f6539ba2fb677b82e0f7f97a728acfa7a9
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-08 22:44:27 +0900

    :sparkles: ADD: middleware example

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+"use strict";
+
 const port = 3000,
   express = require("express"),
   app = express();
+
+app.use((req, res, next) => {
+  cosole.log(`request made to: ${req.url}`);
+  next();
+});
 
 app.get("/items/:vegetable", (req, res) => {
   let veg = req.params.vegetable;

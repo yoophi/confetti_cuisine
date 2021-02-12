554b1fbbb2bbc1f03e9f61f76797ef9b2544f5d8
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-12 22:52:37 +0900

    ✨ UPDATE: handle 404, 500 error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,12 +10,12 @@ exports.logErrors = (error, req, res, next) => {
 exports.respondNoResourceFound = (req, res) => {
   let errorCode = StatusCodes.NOT_FOUND;
   res.status(errorCode);
-  res.send(`${errorCode} | The page does not exist!`);
+  res.sendFile(`./public/${errorCode}.html`, { root: "./" });
 };
 
 exports.respondInternalError = (req, res) => {
   let errorCode = StatusCodes.INTERNAL_SERVER_ERROR;
   console.log(`ERROR occurred: ${error.stack}`);
   res.status(errorCode);
-  res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+  res.sendFile(`./public/${errorCode}.html`, { root: "./" });
 };

28ee90b6d617c6aef981b188bafac0ba592c7bd5
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-03 23:53:56 +0900

    :sparkles: ADD: api route

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -23,11 +23,7 @@ module.exports = {
       });
   },
   indexView: (req, res) => {
-    if (req.query.format === "json") {
-      res.json(res.locals.courses);
-    } else {
-      res.render("courses/index");
-    }
+    res.render("courses/index");
   },
   new: (req, res) => {
     res.render("courses/new");
@@ -45,12 +41,6 @@ module.exports = {
         console.log(`Error saving course: ${error.message}`);
         next(error);
       });
-  },
-
-  redirectView: (req, res, next) => {
-    let redirectPath = res.locals.redirect;
-    if (redirectPath !== undefined) res.redirect(redirectPath);
-    else next();
   },
 
   show: (req, res, next) => {
@@ -113,5 +103,67 @@ module.exports = {
         console.log(`Error deleting course by ID: ${error.message}`);
         next();
       });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath !== undefined) res.redirect(redirectPath);
+    else next();
+  },
+  respondJSON: (req, res) => {
+    res.json({
+      status: httpStatus.OK,
+      data: res.locals,
+    });
+  },
+  errorJSON: (error, req, res, next) => {
+    let errorObject;
+    if (error) {
+      errorObject = {
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    } else {
+      errorObject = {
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: "Unknown Error.",
+      };
+    }
+    res.json(errorObject);
+  },
+  join: (req, res, next) => {
+    let courseId = req.params.id,
+      currentUser = req.user;
+    if (currentUser) {
+      User.findByIdAndUpdate(currentUser, {
+        $addToSet: {
+          courses: courseId,
+        },
+      })
+        .then(() => {
+          res.locals.success = true;
+          next();
+        })
+        .catch((error) => {
+          next(error);
+        });
+    } else {
+      next(new Error("User must log in."));
+    }
+  },
+  filterUserCourses: (req, res, next) => {
+    let currentUser = res.locals.currentUser;
+    if (currentUser) {
+      let mappedCourses = res.locals.courses.map((course) => {
+        let userJoined = currentUser.courses.some((userCourse) => {
+          return userCourse.equals(course._id);
+        });
+        return Object.assign(course.toObject(), { joined: userJoined });
+      });
+      res.locals.courses = mappedCourses;
+      next();
+    } else {
+      next();
+    }
   },
 };
